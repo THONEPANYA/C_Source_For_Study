770e6ccc4bdfef0568bd197921a12065f4ce401c
THONEPANYA <sihapanya.inth@gmail.com>
2024-05-10 23:55:03 +0700

    Create Project.c

---
+++ Project.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// printf (format string , list of data)
+
+int main()
+{
+	printf("Student Data\n");
+	printf("FirstName = %s , LastName = %s", "SIHAPANYA", "INTHONE\n");
+	printf("Student ID = %s", "00000001");
+	return 0;
+}
